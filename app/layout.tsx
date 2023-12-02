b66a412bcd20a0e0c09d3ce2fc6b9a66403c7e08
Gio <misael_marin@dlsu.edu.ph>
2023-12-02 17:41:16 +0800

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,6 @@ export default function RootLayout({
 			<body className={inter.className}>
 				<div className="flex flex-col">
 					<Link href="/auth-server-action">Auth Server Action</Link>
-					<Link href="/">home</Link>
 					<Link href="/todo">Learn CRUD</Link>
 				</div>
 				{children} <Toaster />
